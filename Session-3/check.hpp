6c04c623666c801360d7e6bb2dbc27373f084b3d
Hugo Brunie <hbrunie0@gmail.com>
2020-06-18 11:20:46 -0700

    rename files

--- Session-3/check.hpp
+++
@@ -1,15 +0,0 @@
-//macro for checking errors thrown by CUDA API from https://github.com/olcf/cuda-training-series
-#define cudaCheck(msg) \
-    do { \
-        cudaError_t __err = cudaGetLastError(); \
-        if (__err != cudaSuccess) { \
-            fprintf(stderr, "Fatal error: %s (%s at %s:%d)\n", \
-                msg, cudaGetErrorString(__err), \
-                __FILE__, __LINE__); \
-            fprintf(stderr, "*** FAILED - ABORTING\n"); \
-            exit(1); \
-        } \
-    } while (0)
-// control verbose printf statements 
-#define verbprintf(LEVEL, ...)                                                           \     if(verbose_level >= LEVEL)                                                           \         fprintf(stderr, __VA_ARGS__);
-
