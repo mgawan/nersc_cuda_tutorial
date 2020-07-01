18d60c67dc113fc1da7b20bf24d79a9b60c806c6
Hugo Brunie <hbrunie0@gmail.com>
2020-07-01 15:07:54 -0700

    -n1

--- Session-2/transactions.sh
+++ Session-2/transactions.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-srun nv-nsight-cu-cli --metrics l1tex__t_sectors_pipe_lsu_mem_global_op_ld.sum,l1tex__t_requests_pipe_lsu_mem_global_op_ld.sum ./vec_add
+srun -n1 nv-nsight-cu-cli --metrics l1tex__t_sectors_pipe_lsu_mem_global_op_ld.sum,l1tex__t_requests_pipe_lsu_mem_global_op_ld.sum ./vec_add
